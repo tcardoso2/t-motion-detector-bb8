17b622fb7a9922120fc64fe84336cfde1fa398e6
tercRPi3 <tcardoso@gmail.com>
2018-10-09 23:39:43 +0800

    Added manual test

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
 var entities = require('./Entities');
+var bb8 = require('./main');
+var vermon = require('vermon');
 
-var n = new entities.BB8Notifier('D2:2E:2E:86:20:08');
+vermon.use(bb8);
+
+var n = new entities.BB8Notifier('my BB8', 'E7:94:DC:54:AB:B7');
+//'D2:2E:2E:86:20:08');
+n.notify("Some text", undefined, { command: "roll", args: [150, 200]});
+
