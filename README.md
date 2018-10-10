3390ca66232d4e61d10937a3ff9e12894b45b013
Tiago Cardoso <tcardoso@gmail.com>
2018-10-10 10:05:26 +0800

    Simplified Extending Entities

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # t-motion-detector-bb8
 An Extension of the t-motion-detector library for controlling the Sphero BB8 via BLE
 
+ * v 0.2.2: Simplified extending Entities, updated dependency to vermon@0.6.6;  
  * v 0.2.1: Updated dependency to vermon@0.6.5;  
  * v 0.2.0: Updated to vermon library (WIP), using standard shpero dependency instead of cylon;  
  * v 0.1.2: Added cylon packages as dependencies, added first Unit tests;  
--- main.js
+++ main.js
@@ -1,28 +1,10 @@
-var vermon = require('vermon');
+let BB8Notifier = require('./Entities').ent.BB8Notifier;
 
-class Config extends vermon.Config {
-
-	constructor()
-	{
-		super();
-		//FIXME: This is not ideal, I had to repeat the local binding because 
-		//       it was defaulting to the parent one, will check for an improvement 
-		//       later
-		try{
-          this.file = require('./local.js');
-        } catch (e)
-        {
-          this.file = require('./config.js');
-        }
-	}
-
-	bB8Address()
-	{
-		return "xxx";
-	}
-}
-
-exports.Config = Config;
 exports.id = module.id;
-exports.PreAddPlugin = () => {};
-exports.PostAddPlugin = () => {};
+exports.PreAddPlugin = (vermon, factory) => {
+	//Extending Entities Factory
+	factory.extend({BB8Notifier});
+};
+exports.PostAddPlugin = () => {
+};
+
